3a231858973ce8c657787968c362ffffba1e2ea4
Nedifinita <noxylva@gmail.com>
2025-07-29 16:21:42 +0800

    init

---
+++ dist/pkg/transit_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const stop_s: () => void;
+export const init_d: (a: number, b: number) => [number, number];
+export const init_scd: (a: number, b: number) => [number, number];
+export const get_stn: () => [number, number, number, number];
+export const find: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number];
+export const main: () => void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
