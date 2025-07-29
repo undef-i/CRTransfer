7a97de95be0fbef947adf50778c92573ff6dd541
Nedifinita <noxylva@gmail.com>
2025-07-29 20:11:57 +0800

    refactor: optimize the pathfinding algorithm

--- src/wasm/lib.rs
+++ src/wasm/lib.rs
@@ -56,14 +56,19 @@ thread_local! {
 
 #[derive(Debug, Clone)]
 struct St {
-    tdur: i32, aat: i32, sid: usize, idt: i32,
+    tdur: i32, aat: i32, sid: usize, idt: i32, x: i32,
     p: Option<Rc<St>>, r: Option<R>,
 }
 
 impl PartialEq for St { fn eq(&self, o: &Self) -> bool { self.tdur == o.tdur } }
 impl Eq for St {}
 impl PartialOrd for St { fn partial_cmp(&self, o: &Self) -> Option<Ordering> { Some(self.cmp(o)) } }
-impl Ord for St { fn cmp(&self, o: &Self) -> Ordering { o.tdur.cmp(&self.tdur).then_with(|| o.aat.cmp(&self.aat)) } }
+impl Ord for St {
+    fn cmp(&self, o: &Self) -> Ordering {
+        o.tdur.cmp(&self.tdur)
+         .then_with(|| self.idt.cmp(&o.idt))
+    }
+}
 
 #[wasm_bindgen]
 pub fn stop_s() { STOP.with(|s| { *s.borrow_mut() = true; }); }
@@ -122,7 +127,7 @@ fn pre_scd(s: &str) -> Result<HashMap<usize, Vec<usize>>, String> {
 
     let root: Root = serde_json::from_str(s).map_err(|e| format!("json err: {}", e))?;
     
-
+    
     let mut s2g: HashMap<usize, Vec<usize>> = HashMap::new();
     for grp in root.g {
         let sids: Vec<usize> = grp.s.iter().map(|s| get_oc_sid(&s.n)).collect();
@@ -148,42 +153,24 @@ fn pre_dat(s: &str) -> Result<HashMap<usize, Vec<R>>, String> {
     #[derive(Deserialize)] struct Root { t: Vec<T> }
 
     let root: Root = serde_json::from_str(s).map_err(|e| format!("json err: {}", e))?;
-
     let mut rfs: HashMap<usize, Vec<R>> = HashMap::with_capacity(1000);
 
     for train in root.t {
-        if train.s.len() < 2 { continue; }
-        
-        let mut abs_stops = Vec::new();
-        if let Some(first) = train.s.first() {
-            let mut dep = first.d;
-            if dep < first.a { dep += 1440; }
-            let sid = get_oc_sid(&first.n);
-            abs_stops.push((sid, first.a, dep));
+        for i in 0..(train.s.len().saturating_sub(1)) {
+            let b_stop = &train.s[i];
+            let a_stop = &train.s[i + 1];
 
-            for i in 1..train.s.len() {
-                let prev_abs = &abs_stops[i - 1];
-                let prev_tpl = &train.s[i - 1];
-                let curr_tpl = &train.s[i];
-                let mut leg_dur = curr_tpl.a - prev_tpl.d;
-                if leg_dur < 0 { leg_dur += 1440; }
-                let curr_arr = prev_abs.2 + leg_dur;
-                let mut wait_dur = curr_tpl.d - curr_tpl.a;
-                if wait_dur < 0 { wait_dur += 1440; }
-                let curr_dep = curr_arr + wait_dur;
-                let sid = get_oc_sid(&curr_tpl.n);
-                abs_stops.push((sid, curr_arr, curr_dep));
-            }
-        }
-
-        for i in 0..(abs_stops.len() - 1) {
-            let b_stop = &abs_stops[i];
-            let a_stop = &abs_stops[i + 1];
-            let dtr = train.s[i].d;
-            let dur = a_stop.1 - b_stop.2;
-            if dur >= 0 {
-                rfs.entry(b_stop.0).or_insert_with(Vec::new).push(R {
-                    tn: train.tn.clone(), bs: b_stop.0, al: a_stop.0, dtr, dur,
+            if b_stop.d != -1 && a_stop.a != -1 && a_stop.a > b_stop.d {
+                let b_sid = get_oc_sid(&b_stop.n);
+                let a_sid = get_oc_sid(&a_stop.n);
+                let duration = a_stop.a - b_stop.d;
+                
+                rfs.entry(b_sid).or_insert_with(Vec::new).push(R {
+                    tn: train.tn.clone(),
+                    bs: b_sid,
+                    al: a_sid,
+                    dtr: b_stop.d,
+                    dur: duration,
                 });
             }
         }
@@ -218,13 +205,13 @@ pub fn find(o: &str, d: &str, mtt: i32, esc_o: bool, esc_d: bool) -> Result<(), 
     let d_set = if esc_d { get_sgrp(d_id).into_iter().collect::<HashSet<usize>>() } else { [d_id].into_iter().collect::<HashSet<usize>>() };
     
     let mut pq = BinaryHeap::with_capacity(50000);
-    let mut v: HashMap<usize, Vec<(i32, i32)>> = HashMap::with_capacity(5000);
+    let mut v: HashMap<usize, Vec<(i32, i32, i32)>> = HashMap::with_capacity(5000);
     
     for &o_sid in &o_sids {
         if let Some(rides) = rfs.get(&o_sid) {
             for r in rides {
                 if let Some(aat) = r.dtr.checked_add(r.dur) {
-                    pq.push(St { tdur: r.dur, aat, sid: r.al, idt: r.dtr, p: None, r: Some(r.clone()) });
+                    pq.push(St { tdur: r.dur, aat, sid: r.al, idt: r.dtr, x: 0, p: None, r: Some(r.clone()) });
                 }
             }
         }
@@ -232,30 +219,33 @@ pub fn find(o: &str, d: &str, mtt: i32, esc_o: bool, esc_d: bool) -> Result<(), 
     
     while let Some(c) = pq.pop() {
         if is_stop() { break; }
-        let c_arr = c.aat; let c_dep = c.idt;
-        if let Some(prof) = v.get(&c.sid) {
-            if prof.iter().any(|&(a, d)| a <= c_arr && d >= c_dep) { continue; }
-        }
-        let prof = v.entry(c.sid).or_default();
-        prof.retain(|&(a, d)| !(c_arr <= a && c_dep >= d));
-        prof.push((c_arr, c_dep));
+
         if d_set.contains(&c.sid) {
             let p = rec_path(&c);
-            let jny = Jny { tdur: c.tdur, aat: c.aat, idt: c.idt, x: (p.len() as i32 - 1).max(0), p, };
+            let jny = Jny { tdur: c.tdur, aat: c.aat, idt: c.idt, x: c.x, p, };
             if let Ok(j_json) = serde_json::to_string(&jny) { on_jny(&j_json); }
             continue;
         }
+
+        let c_arr = c.aat; let c_dep = c.idt; let c_x = c.x;
+        if let Some(prof) = v.get(&c.sid) {
+            if prof.iter().any(|&(a, d, x)| a <= c_arr && d >= c_dep && x <= c_x) { continue; }
+        }
+        let prof = v.entry(c.sid).or_default();
+        prof.retain(|&(a, d, x)| !(c_arr <= a && c_dep >= d && c_x <= x));
+        prof.push((c_arr, c_dep, c_x));
+        
         if let Some(next_rs) = rfs.get(&c.sid) {
             for next_r in next_rs {
                 if is_stop() { return Ok(()); }
                 let is_cont = if let Some(ref prev_r) = c.r { prev_r.tn == next_r.tn } else { false };
                 let wait = {
                     let arr_rem = c.aat % 1440;
-                    let dep_rem = next_r.dtr;
+                    let dep_rem = next_r.dtr % 1440;
                     let base_wait = calc_wait(arr_rem, dep_rem);
 
                     if is_cont {
-                        if base_wait < 0 { continue; } else { base_wait }
+                        base_wait
                     } else {
                         let mut calc_w = base_wait;
                         while calc_w < mtt {
@@ -271,7 +261,8 @@ pub fn find(o: &str, d: &str, mtt: i32, esc_o: bool, esc_d: bool) -> Result<(), 
                 };
                 let next_aat = match c.aat.checked_add(wait).and_then(|x| x.checked_add(next_r.dur)) { Some(val) => val, None => continue };
                 let new_tdur = match next_aat.checked_sub(c.idt) { Some(val) => val, None => continue };
-                pq.push(St { tdur: new_tdur, aat: next_aat, sid: next_r.al, idt: c.idt, p: Some(Rc::new(c.clone())), r: Some(next_r.clone()) });
+                let new_x = if is_cont { c.x } else { c.x + 1 };
+                pq.push(St { tdur: new_tdur, aat: next_aat, sid: next_r.al, idt: c.idt, x: new_x, p: Some(Rc::new(c.clone())), r: Some(next_r.clone()) });
             }
         }
     }
